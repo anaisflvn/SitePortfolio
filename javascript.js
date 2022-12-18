462b69d743a77a82d3a80713618024825af2b048
Anaïs <113765044+anaisflvn@users.noreply.github.com>
2022-12-18 14:34:59 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -13,3 +13,17 @@ btnLight.addEventListener('click', function() {
 });
 
 console.log(btns);
+
+window.addEventListener('load', () => {
+  
+  const TL = gsap.timeline({paused: true});
+  
+  TL
+  .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+  .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+  .from(logo, 0.4, {transform: "scale(0)", ease: "power2.out"}, 0.3, '-=2');
+  .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1');
+  
+  
+  Tl.play();
+}) 
